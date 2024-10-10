8a4c1a4cd744ea74b78bb49ecadc59dee388341a
David MTZD <davidmtzd21512@gmail.com>
2024-10-10 17:57:44 -0600

    se agrego un buscar por lenguaje de programacion,buscar por leguaje de programacion y nombre ademas de como hacer la consulta con Query para un nombre del metodo personalizado

--- src/main/java/com/david/springboot/app/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/david/springboot/app/springboot_jpa/repositories/PersonRepository.java
@@ -3,7 +3,16 @@ package com.david.springboot.app.springboot_jpa.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.david.springboot.app.springboot_jpa.entities.Person;
+import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+    List<Person> findByProgramingLanguage(String programingLanguage);
+
+    /*
+     * @Query("select p from Person p where p.programingLanguage=?1 and p.name=?2")
+     * List<Person> buscarByProgramingLanguageAndname(String programingLanguage,
+     * String name);
+     */
+    List<Person> findByProgramingLanguageAndName(String programingLanguage, String name);
 }
