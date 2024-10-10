0d020935050ca5eaf529b7fc25fc3a72c3040c94
David MTZD <davidmtzd21512@gmail.com>
2024-10-10 17:56:36 -0600

    se agrego un buscar por leguaje de programacion y nombre

--- src/main/java/com/david/springboot/app/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/david/springboot/app/springboot_jpa/SpringbootJpaApplication.java
@@ -22,8 +22,9 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		List<Person> persons = (List<Person>) repository.findAll();
+		System.out.println("Consulta:");
+		// List<Person> persons = (List<Person>) repository.findAll();
+		List<Person> persons = (List<Person>) repository.findByProgramingLanguageAndName("Java", "Andres");
 		persons.stream().forEach(person -> System.out.println(person));
 	}
 
