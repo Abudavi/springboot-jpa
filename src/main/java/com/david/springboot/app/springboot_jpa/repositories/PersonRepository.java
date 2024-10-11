233fae43b2287f552d07c623e5a7a0fdd2305eb8
David MTZD <davidmtzd21512@gmail.com>
2024-10-10 18:21:24 -0600

    se agregaron mas querys

--- src/main/java/com/david/springboot/app/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/david/springboot/app/springboot_jpa/repositories/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.david.springboot.app.springboot_jpa.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.david.springboot.app.springboot_jpa.entities.Person;
@@ -9,10 +10,17 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
 
     List<Person> findByProgramingLanguage(String programingLanguage);
 
-    /*
-     * @Query("select p from Person p where p.programingLanguage=?1 and p.name=?2")
-     * List<Person> buscarByProgramingLanguageAndname(String programingLanguage,
-     * String name);
-     */
+    @Query("select p from Person p where p.programingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgramingLanguageAndname(String programingLanguage, String name);
+
     List<Person> findByProgramingLanguageAndName(String programingLanguage, String name);
+
+    @Query("Select p.name, p.programingLanguage from Person p")
+    List<Object[]> obtenerPersoData();
+
+    @Query("select p from Person p where p.lastname=?1")
+    List<Object[]> obtenerPersonaLastname(String lastname);
+
+    @Query("Select p.name from Person p")
+    List<Object[]> obtenerPersoName();
 }
