acd13bac2c2092d2d143401baee463cee72417c6
David MTZD <davidmtzd21512@gmail.com>
2024-10-20 16:46:03 -0600

    se agrego el update

--- src/main/java/com/david/springboot/app/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/david/springboot/app/springboot_jpa/SpringbootJpaApplication.java
@@ -26,7 +26,34 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Consulta:");
-		create();
+		update();
+
+	}
+
+	@Transactional
+	public void update() {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id a la persona a editar");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		/* optionalPerson.ifPresent(person -> { */
+		if (optionalPerson.isPresent()) {
+			Person person = optionalPerson.orElseThrow();
+			System.out.println(person);
+			System.out.println("Ingrese el lenguaje de programacion");
+			String programigLanguage = scanner.next();
+			person.setProgramingLanguage(programigLanguage);
+			Person personDb = repository.save(person);
+			System.out.println(personDb);
+
+		} else {
+			System.out.println("El usuario no existe !");
+		}
+
+		// });
 
 	}
 
